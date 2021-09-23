e97f76a4543ccc58923ede7cdee83b96323d8ae2
Abdullah Aladham <abdullah.aladham@gmail.com>
2021-09-23 13:29:56 +0300

    first commit

---
+++ bookstore/services/process.php
@@ -0,0 +1,11 @@
+<?php
+$server = 'localhost';
+$username = 'root';
+$db = 'crud';
+$pw = '';
+
+$conn = new mysqli($server, $username, $db);
+if ($conn->connect_error) {
+    die('Connection failed:' . $conn->connect_error);
+}
+echo 'connected successfully';
